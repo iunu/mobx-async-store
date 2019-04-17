b5c238913997d6dad3f393e6764a1164a5bb71a9
Jon Dayton <jondayton@gmail.com>
2019-04-17 19:13:28 -0400

    add docs for first methods

---
+++ src/model/index.js
@@ -0,0 +1,172 @@
+/**
+ * Defines attributes that will be serialized and deserialized. Takes one argument, a class that the attribute will be coerced to.
+ * This can be a Javascript primitive or another class. `id` cannot be defined as it is assumed to exist.
+ * Attributes can be defined with a default.
+ * ```
+ * class CalendarEvent extends Model {
+ *   @attribute(Date) start_time = moment()
+ * }
+ * ```
+ * @method attribute
+ * @
+ */
+
+export function attribute (coerce = (obj) => obj) {
+  return function (target, property, descriptor) {
+    coerce(property)
+    descriptor.property = property
+    return descriptor
+  }
+}
+
+/**
+ * Defines a one-to-many relationship. Defaults to the class with camelized singular name of the property
+ * An optional argument specifies the data model, if different from the property name
+ * ```
+ * class CropVariety extends Model {
+ *   @hasMany growth_cycles
+ * }
+ *
+ * class Crop extends Model {
+ *   @hasMany(CropVariety) varieties
+ * }
+ * ```
+ * @method hasMany
+ */
+
+/**
+ * Defines a many-to-one relationship. Defaults to the class with camelized name of the property.
+ * An optional argument specifies the data model, if different from the property name.
+ * ```
+ * class CropVariety extends Model {
+ *   @belongsTo crop
+ *   @belongsTo(Facility) greenhouse
+ * }
+ * ```
+ * Polymorphic relationships
+ * Define `belongsTo` with the the associated models
+ * Define `hasMany` as you normally would
+ * ```
+ * class Note extends Model {
+ *   @belongsTo(CalendarEvent,Kpi,CropBatchAction) notable
+ * }
+ *
+ * class CalendarEvent extends Model {
+ *   @hasMany notes
+ * }
+ * ```
+ * @method belongsTo
+ */
+
+/**
+ @class Model
+ */
+
+class Model {
+  /**
+   * The type of the model. Defined on the class. Defaults to the underscored version of the class name
+   * (eg 'calendar_events').
+   *
+   * @property type
+   * @static
+   */
+
+  /**
+   * The canonical path to the resource on the server. Defined on the class.
+   * Defaults to the underscored version of the class name
+   * @property endpoint
+   * @static
+   */
+
+  /**
+   * True if the instance has been modified from its persisted state
+   * ```
+   * kpi = store.find('kpis', 5)
+   * kpi.name
+   * => "A good thing to measure"
+   * kpi.isDirty
+   * => false
+   * kpi.name = "Another good thing to measure"
+   * kpi.isDirty
+   * => true
+   * await kpi.save()
+   * kpi.isDirty
+   * => false
+   * ```
+   * @property isDirty
+   * @type {Boolean}
+   * @default false
+   */
+
+  /**
+   * True if the instance is coming from / going to the server
+   * ```
+   * kpi = store.find('kpis', 5)
+   * // fetch started
+   * kpi.isInFlight
+   * => true
+   * // fetch finished
+   * kpi.isInFlight
+   * => false
+   * ```
+   * @property isInFlight
+   * @type {Boolean}
+   * @default false
+   */
+
+  /**
+   * A hash of errors from the server
+   * ```
+   * kpi = store.find('kpis', 5)
+   * kpi.errors
+   * => { authorization: "You do not have access to this resource" }
+   * ```
+   * @property errors
+   * @type {Object}
+   * @default {}
+   */
+
+  /**
+   * The current state of defined properties and relationships of the instance
+   * ```
+   * kpi = store.find('kpis', 5)
+   * kpi.name
+   * => "A good thing to measure"
+   * snapshot = kpi.snapshot
+   * kpi.name = "Another good thing to measure"
+   * snapshot.name
+   * => "A good thing to measure"
+   * ```
+   * @property snapshot
+   * @type {Object}
+   * @default {}
+   */
+
+  /**
+   * restores data to its last persisted state
+   * ```
+   * kpi = store.find('kpis', 5)
+   * kpi.name
+   * => "A good thing to measure"
+   * kpi.name = "Another good thing to measure"
+   * kpi.rollback()
+   * kpi.name
+   * => "A good thing to measure"
+   * ```
+   * @method rollback
+   */
+
+  /**
+   * creates or updates a record.
+   * @method save
+   * @return {Promise}
+   */
+
+  /**
+   * deletes a record from the store and server
+   * @method destroy
+   * @return {Promise} an empty promise with any success/error status
+   */
+}
+
+export default Model
--- src/store.js
+++ src/store.js
@@ -10,8 +10,104 @@ class Store {
   @observable data = {}
 
   /**
-   * Method for smoke test
+   * Adds an instance or an array of instances to the store.
+   * ```
+   * kpiHash = { name: "A good thing to measure" }
+   * kpi = store.add('kpis', kpiHash)
+   * kpi.name
+   * => "A good thing to measure"
+   * ```
+   * @method add
+   * @param {Object} properties the properties to use
+   * @return {Object} the new record
+   */
+
+  /**
+   * finds an instance by `id`. If available in the store, returns that instance. Otherwise, triggers a fetch.
    *
+   *   store.findOne('calendar_events', 5)
+   *   // fetch triggered
+   *   => event1
+   *   store.findOne('calendar_events', 5)
+   *   // no fetch triggered
+   *   => event1
+   *
+   * Passing `fromServer` as an option will always trigger a fetch if `true` and never trigger a fetch if `false`.
+   * Otherwise, it will trigger the default behavior
+   *
+   *   store.findOne('calendar_events', 5, { fromServer: false })
+   *   // no fetch triggered
+   *   => undefined
+   *
+   *   store.findOne('calendar_events', 5)
+   *   // fetch triggered
+   *   => event1
+   *
+   *   store.findOne('calendar_events', 5, { fromServer: true })
+   *   // fetch triggered
+   *   => event1
+   *
+   * @method findOne
+   * @param {String} type the type to find
+   * @param id
+   * @param {Object} options
+   */
+
+  /**
+   * finds all of the instances of a given type. If there are instances available in the store,
+   * it will return those, otherwise it will trigger a fetch
+   *
+   *   store.findAll('calendar_events')
+   *   // fetch triggered
+   *   => [event1, event2, event3]
+   *   store.findAll('calendar_events')
+   *   // no fetch triggered
+   *   => [event1, event2, event3]
+   *
+   * passing `fromServer` as an option will always trigger a fetch if `true` and never trigger a fetch if `false`.
+   * Otherwise, it will trigger the default behavior
+   *
+   *   store.findAll('calendar_events', { fromServer: false })
+   *   // no fetch triggered
+   *   => []
+   *
+   *   store.findAll('calendar_events')
+   *   // fetch triggered
+   *   => [event1, event2, event3]
+   *
+   *   // async stuff happens on the server
+   *   store.findAll('calendar_events')
+   *   // no fetch triggered
+   *   => [event1, event2, event3]
+   *
+   *   store.findAll('calendar_events', { fromServer: true })
+   *   // fetch triggered
+   *   => [event1, event2, event3, event4]
+   *
+   * Query params can be passed as part of the options hash. The response will be cached, so the next time `findAll`
+   * is called with identical params and values, the store will first look for the local result (unless `fromServer` is `true`)
+   *
+   *   store.findAll('calendar_events', {
+   *     queryParams: { start_time: moment(), end_time: moment() }
+   *   })
+   *
+   * @method findAll
+   * @param {String} type the type to find
+   * @param {Object} options
+   */
+
+  /**
+   * Clears the store of a given type, or clears all if no type given
+   *
+   *   store.reset('calendar_events')
+   *   // removes all calendar_events from store
+   *   store.reset()
+   *   // clears store
+   *
+   * @method reset
+   */
+
+  /**
    * @method ping
    * @return {String} 'pong'
    */
