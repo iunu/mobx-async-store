42280d4f09d55978a266d122c3d3f823f783ce6b
Jon Dayton <jdayton@agrilyst.com>
2019-04-21 12:34:41 +0100

    add circleci config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": [
+    "standard"
+  ],
+  "rules": {
+    "camelcase": 0,
+    "indent": 0
+  },
+  "env": {
+    "jest": true
+  }
+};
