9cef222fba585a1c1304a1863d1a622e0e1efcce
Jon Dayton <jondayton@gmail.com>
2020-01-29 14:21:46 -0500

    add documentation for github pages

---
+++ doc/api.js
@@ -0,0 +1,13 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Model",
+        "RelatedRecordsArray",
+        "Schema",
+        "Store"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
