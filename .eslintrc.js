146f0070b89a757e01c4f78d7eece0d8e1b599a4
Jordan Stephens <jordan@stephens.io>
2020-05-04 16:43:12 -0500

    compatible eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,11 @@ module.exports = {
   ],
   "rules": {
     "camelcase": 0,
+    "curly": 0,
+    "comma-dangle": 0,
     "indent": 0,
-    "quote-props": [2, "consistent-as-needed"]
+    "quote-props": [2, "consistent-as-needed"],
+    "space-before-function-paren": 0
   },
   "env": {
     "jest": true
