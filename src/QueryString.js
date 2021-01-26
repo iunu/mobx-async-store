832dea4bc3e394c451593488aaa6515d9fe85f74
Daniel <daniel.ashcraft@ofashandfire.com>
2021-01-25 21:54:51 -0600

    another small update

--- src/QueryString.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var qs_1 = require("qs");
-var QueryString = {
-    parse: function (str) { return qs_1["default"].parse(str, { ignoreQueryPrefix: true }); },
-    stringify: function (str) { return qs_1["default"].stringify(str, { arrayFormat: 'brackets' }); }
-};
-exports["default"] = QueryString;
